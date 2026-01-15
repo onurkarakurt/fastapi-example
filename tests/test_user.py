8fc3ed7d8eb52a15609ee744b4abba346689a356
onurkarakurt <onurkarakurt93@gmail.com>
2026-01-15 17:34:29 +0300

    login failed test added

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,3 +26,17 @@ def test_login_user(client, test_user):
     assert id == test_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("wrongemail@gmail.com", "password123", 403),
+    ("hello@gmail.com", "wrongpassword", 403),
+    ("wrongemail@gmail.com", "wrongpassword", 403),
+    (None, "password123", 403),
+    ("hello@gmail.com", None, 403)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login",
+        data={"username": email, "password": password}
+    )
+    assert res.status_code == status_code
